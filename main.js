e5ae6e736bd8d4e813d0eaece8cdb0a71df5b450
phwoo0901 <phwoo1004@gmail.com>
2021-07-23 17:50:20 +0900

    Express study day1 홈페이지 구현

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ var fs = require('fs');
 var template = require('./lib/template.js');
 
 // get : route, routing
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(requset, response) {
+  fs.readdir('./data', function(error, filelist) {
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    response.send(html);
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
