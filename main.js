ce85a1a15c3ecfeacaa21be1fb9e854b69849c22
phwoo0901 <phwoo1004@gmail.com>
2021-07-24 22:08:19 +0900

    Express study day2 Use Middleware (compression)

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 // Middleware - body parser
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Middleware - compression
+app.use(compression());
 
 // get : route, routing
 // app.get('/', (req, res) => res.send('Hello World!'))
@@ -78,22 +82,6 @@ app.post('/create_process', function(request, response) {
     response.writeHead(302, {Location: `/page/${title}`});
     response.end();
   });
-
-  /*
-  var body = '';
-  request.on('data', function(data) {
-      body = body + data;
-  });
-  request.on('end', function() {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-      response.writeHead(302, {Location: `/page/${title}`});
-      response.end();
-    });
-  });
-  */
 });
 
 app.get('/update/:pageId', function(request, response) {
@@ -131,24 +119,6 @@ app.post('/update_process', function(request, response) {
       response.redirect(`/page/${title}`);
     });
   });
-
-  /*
-  var body = '';
-  request.on('data', function(data) {
-      body = body + data;
-  });
-  request.on('end', function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error) {
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-        response.redirect(`/page/${title}`);
-      });
-    });
-  });
-  */
 });
 
 app.post('/delete_process', function(request, response) {
@@ -158,21 +128,6 @@ app.post('/delete_process', function(request, response) {
   fs.unlink(`data/${filteredId}`, function(error) {
     response.redirect('/');
   });
-
-  /*
-  var body = '';
-  request.on('data', function(data) {
-      body = body + data;
-  });
-  request.on('end', function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error) {
-      response.redirect('/');
-    });
-  });
-  */
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
